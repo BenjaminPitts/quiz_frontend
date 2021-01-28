6f1ed9eb516619de9957ca10daef3077ef579867
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-01-28 12:15:43 -0700

    frontend boilerplate

---
+++ quiz/quiz_frontend/frontend/src/components/Quiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Quiz extends Component {
+render = () => {
+  return (
+    <div className='quiz'>
+    <h2>Quiz Schema</h2>
+    </div>
+    )
+  }
+}
+
+
+export default Quiz
