08649f7704cbab9371682f6e074eafa0999c4aa9
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-01-31 11:28:10 -0700

    quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react'
 class Quiz extends Component {
   render = () => {
     return (
-    <>
-          <div className='update'>
+      <div className='update'>
           <h4>Question: {this.props.quiz.question}</h4>
           <h5>Answer: {this.props.quiz.answer}</h5>
+        <form onSubmit={this.props.isTrue}>
+          <label>Your Answer:</label>
+          <input type='text' id={this.props.quiz.id} onChange={this.props.handleChange} />
+          <input type='submit' value='Submit Answer' />
+        </form>
 
-          <details>
+        <details>
           <summary>Edit Question: </summary>
           <form id={this.props.quiz.id} onSubmit={this.props.updateQuestion}>
           <label htmlFor='question'>Q:</label>
@@ -19,15 +23,24 @@ class Quiz extends Component {
           <br />
           <input type='text' id='answer' onChange={this.props.handleChange} />
           <br />
+          <label htmlFor='answer_char'>A_char:</label>
+          <input type='text' id='answer_char' onChange={this.props.handleChange} value={this.props.answer_char} />
+          <br />
+          <label htmlFor='point_value'>P:</label>
+          <input type='text' id='point_value' onChange={this.props.handleChange} value={this.props.point_value} />
+          <br />
           <input type='submit' value='Update Question' />
           <br />
           <button value={this.props.quiz.id} onClick={this.props.deleteQuestion}>
           DELETE QUESTION</button>
-          </form>
-          </details>
 
-          </div>
-    </>
+          </form>
+        </details>
+        <button value={this.props.quiz.id} onClick={this.props.showAnswer}>
+        Show Answer</button>
+        <br />
+
+      </div>
     )
   }
 }
